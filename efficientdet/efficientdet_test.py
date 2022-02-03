534c7f4989a96fd1f8df068f828a139d7cd2bade
Xoi Ga <xoiga123@outlook.com.vn>
2022-02-03 19:05:16 +0700

    val from cmd, fix demo

--- efficientdet/efficientdet_test.py
+++ efficientdet/efficientdet_test.py
@@ -17,19 +17,18 @@ import glob
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, plot_one_box
 
-compound_coef = 1
+compound_coef = 3
 force_input_size = None  # set None to use default size
-# img_path = ['demo_imgs/' + i for i in os.listdir('demo_imgs')]
 img_path = [path for path in glob.glob('./demo_imgs/*.jpg')]
-# print(img_path)
-img_path = img_path[3]
+print(img_path)
+img_path = [img_path[0]]
 
 # replace this part with your project's anchor config
-anchor_ratios = [(0.7, 1.4), (1.0, 1.0), (1.3, 0.8)]
-anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
+anchor_ratios = [(0.62, 1.58), (1.0, 1.0), (1.58, 0.62)]
+anchor_scales = [2**0, 2**0.70, 2**1.32]
 
-threshold = 0.2
-iou_threshold = 0.2
+threshold = 0.5
+iou_threshold = 0.3
 
 use_cuda = True
 use_float16 = False
@@ -52,10 +51,10 @@ obj_list= ['car']
 
 color_list = standard_to_bgr(STANDARD_COLORS)
 # tf bilinear interpolation is different from any other's, just make do
-input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536, 1536]
+input_sizes = [512, 640, 768, 640, 1024, 1280, 1280, 1536, 1536]
 input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
 ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
-ori_img = ori_imgs[0]
+# ori_img = ori_imgs[0]
 
 if use_cuda:
     x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
@@ -63,13 +62,14 @@ else:
     x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
 
 x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
+print(x.shape)
 
 model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
                              ratios=anchor_ratios, scales=anchor_scales, seg_classes =2)
 try:
-    model.load_state_dict(torch.load('/home/pingu/PycharmProjects/FinalProject/FinalProject/efficientdet/checkpoints/bdd100k_effdet/efficientdet-d1_best.pth', map_location='cpu'))
+    model.load_state_dict(torch.load('weights/weight97.pth', map_location='cpu'))
 except:
-    model.load_state_dict(torch.load('/home/pingu/PycharmProjects/FinalProject/FinalProject/efficientdet/checkpoints/bdd100k_effdet/efficientdet-d1_best.pth', map_location='cpu')['model'])
+    model.load_state_dict(torch.load('weights/weight97.pth', map_location='cpu')['model'])
 model.requires_grad_(False)
 model.eval()
 
@@ -85,6 +85,7 @@ with torch.no_grad():
     # print(ori_img.size())
     # ratio = 640 / 1280
     # da_predict = seg[:, :, 0:(720 - 0), 0:(1280 - 0)]
+    print(seg.shape)
 
     # print(da_predict.shape)
     da_seg_mask = torch.nn.functional.interpolate(seg, size = [720,1280], mode='bilinear')
@@ -94,41 +95,43 @@ with torch.no_grad():
 
     # seg = torch.rand((1, 384, 640))
     # da_seg_mask = Activation('sigmoid')(da_seg_mask)
-    # print(da_seg_mask)
+    print(da_seg_mask.shape)
     color_mask_ls = []
-    # for i in range(da_seg_mask.size(0)):
-    da_seg_mask = da_seg_mask.squeeze().cpu().numpy().round()
-    # da_seg_mask = torch.argmax(da_seg_mask, dim = 0)
-    # da_seg_mask[da_seg_mask < 0.5] = 0
-    # da_seg_mask[da_seg_mask >= 0.5] = 1
+    for i in range(da_seg_mask.size(0)):
+      print(i)
+      da_seg_mask_ = da_seg_mask[i].squeeze().cpu().numpy().round()
+      # da_seg_mask = torch.argmax(da_seg_mask, dim = 0)
+      # da_seg_mask[da_seg_mask < 0.5] = 0
+      # da_seg_mask[da_seg_mask >= 0.5] = 1
 
-    color_area = np.zeros((da_seg_mask.shape[0], da_seg_mask.shape[1], 3), dtype=np.uint8)
+      color_area = np.zeros((da_seg_mask_.shape[0], da_seg_mask_.shape[1], 3), dtype=np.uint8)
 
-    # for label, color in enumerate(palette):
-    #     color_area[result[0] == label, :] = color
+      # for label, color in enumerate(palette):
+      #     color_area[result[0] == label, :] = color
 
-    color_area[da_seg_mask == 1] = [0, 255, 0]
-    color_area[da_seg_mask == 2] = [0, 0, 255]
+      color_area[da_seg_mask_ == 1] = [0, 255, 0]
+      color_area[da_seg_mask_ == 2] = [0, 0, 255]
 
-    color_seg = color_area[..., ::-1]
-    print(color_seg.shape)
+      color_seg = color_area[..., ::-1]
+      print(color_seg.shape)
 
-    # cv2.imwrite('seg.jpg',color_seg)
+      cv2.imwrite('seg_only_{}.jpg'.format(i),color_seg)
 
 
-    # convert to BGR
-    # color_seg = color_seg[..., ::-1]
-    # # print(color_seg.shape)
-    color_mask = np.mean(color_seg, 2)
-    print(ori_img.shape)
-    # ori_img = cv2.resize(ori_img, (1280, 768), interpolation=cv2.INTER_LINEAR)
-    ori_img[color_mask != 0] = ori_img[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
-    # img = img * 0.5 + color_seg * 0.5
-    ori_img = ori_img.astype(np.uint8)
-    # img = cv2.resize(ori_img, (1280, 720), interpolation=cv2.INTER_LINEAR)
-    cv2.imwrite('abababab.jpg', ori_img)
-    # cv2.waitKey(0)
-    color_mask_ls.append(color_mask)
+      # convert to BGR
+      # color_seg = color_seg[..., ::-1]
+      # # print(color_seg.shape)
+      color_mask = np.mean(color_seg, 2)
+      ori_img = ori_imgs[i]
+      print(ori_img.shape)
+      # ori_img = cv2.resize(ori_img, (1280, 768), interpolation=cv2.INTER_LINEAR)
+      ori_img[color_mask != 0] = ori_img[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
+      # img = img * 0.5 + color_seg * 0.5
+      ori_img = ori_img.astype(np.uint8)
+      # img = cv2.resize(ori_img, (1280, 720), interpolation=cv2.INTER_LINEAR)
+      cv2.imwrite('seg_{}.jpg'.format(i), ori_img)
+      # cv2.waitKey(0)
+      color_mask_ls.append(color_mask)
 
     regressBoxes = BBoxTransform()
     clipBoxes = ClipBoxes()
@@ -193,13 +196,13 @@ with torch.no_grad():
     print(f'{tact_time} seconds, {1 / tact_time} FPS, @batch_size 1')
 
     # uncomment this if you want a extreme fps test
-    # print('test2: model inferring only')
-    # print('inferring images for batch_size 32 for 10 times...')
-    # t1 = time.time()
-    # x = torch.cat([x] * 32, 0)
-    # for _ in range(10):
-    #     _, regression, classification, anchors = model(x)
-    #
-    # t2 = time.time()
-    # tact_time = (t2 - t1) / 10
-    # print(f'{tact_time} seconds, {32 / tact_time} FPS, @batch_size 32')
+    print('test2: model inferring only')
+    print('inferring images for batch_size 32 for 10 times...')
+    t1 = time.time()
+    x = torch.cat([x] * 32, 0)
+    for _ in range(10):
+        _, regression, classification, anchors, segmentation = model(x)
+    
+    t2 = time.time()
+    tact_time = (t2 - t1) / 10
+    print(f'{tact_time} seconds, {32 / tact_time} FPS, @batch_size 32')
--- efficientdet/val.py
+++ efficientdet/val.py
@@ -1,15 +1,25 @@
 import torch
 import numpy as np
+import os
+import argparse
+import yaml
+from tqdm.autonotebook import tqdm
+from pathlib import Path
 
 from utils import smp_metrics
 from efficientdet.utils import BBoxTransform, ClipBoxes
-from utils.utils import ConfusionMatrix, postprocess, invert_affine, scale_coords, process_batch, ap_per_class, fitness, \
-    save_checkpoint
-from tqdm.autonotebook import tqdm
+from utils.utils import ConfusionMatrix, preprocess, postprocess, invert_affine, scale_coords, process_batch, ap_per_class, fitness, \
+    save_checkpoint, boolean_string, display
+from backbone import EfficientDetBackbone
+from efficientdet.bdd import BddDataset
+from efficientdet.AutoDriveDataset import AutoDriveDataset
+from efficientdet.yolop_cfg import update_config
+from efficientdet.yolop_cfg import _C as cfg
+from efficientdet.yolop_utils import DataLoaderX
+from torchvision import transforms
 
 
 @torch.no_grad()
-
 def val(model, optimizer, val_generator, params, opt, writer, epoch, step, best_fitness, best_loss, best_epoch):
     model.eval()
     loss_regression_ls = []
@@ -223,3 +233,234 @@ def val(model, optimizer, val_generator, params, opt, writer, epoch, step, best_
     return best_fitness, best_loss, best_epoch
 
 
+@torch.no_grad()
+def val_from_cmd(model, val_generator, params):
+    model.eval()
+    loss_regression_ls = []
+    loss_classification_ls = []
+    loss_segmentation_ls = []
+    jdict, stats, ap, ap_class = [], [], [], []
+    iou_thresholds = torch.linspace(0.5, 0.95, 10).cuda()  # iou vector for mAP@0.5:0.95
+    num_thresholds = iou_thresholds.numel()
+    nc = 1
+    seen = 0
+    plots = True
+    confusion_matrix = ConfusionMatrix(nc=nc)
+    s = ('%15s' + '%11s' * 12) % (
+    'Class', 'Images', 'Labels', 'P', 'R', 'mAP@.5', 'mAP@.5:.95', 'mIoU', 'mF1', 'rIoU', 'rF1', 'lIoU', 'lF1')
+    dt, p, r, f1, mp, mr, map50, map = [0.0, 0.0, 0.0], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    iou_ls = [[] for _ in range(3)]
+    f1_ls = [[] for _ in range(3)]
+    regressBoxes = BBoxTransform()
+    clipBoxes = ClipBoxes()
+
+    val_loader = tqdm(val_generator)    
+    for iter, data in enumerate(val_loader):
+        imgs = data['img']
+        annot = data['annot']
+        seg_annot = data['segmentation']
+        filenames = data['filenames']
+        shapes = data['shapes']
+
+
+        if params['num_gpus'] == 1:
+            imgs = imgs.cuda()
+            annot = annot.cuda()
+            seg_annot = seg_annot.cuda()
+
+        features, regressions, classifications, anchors, segmentation = model(imgs)
+
+        out = postprocess(imgs.detach(),
+                          torch.stack([anchors[0]] * imgs.shape[0], 0).detach(), regressions.detach(),
+                          classifications.detach(),
+                          regressBoxes, clipBoxes,
+                          0.001, 0.6)  # 0.5, 0.3
+
+        # imgs = imgs.permute(0, 2, 3, 1).cpu().numpy()
+        # imgs = ((imgs * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255).astype(np.uint8)
+        # imgs = [cv2.cvtColor(img, cv2.COLOR_RGB2BGR) for img in imgs]
+        # display(out, imgs, ['car'], imshow=False, imwrite=True)
+
+        # for index, filename in enumerate(filenames):
+        #   ori_img = cv2.imread('datasets/bdd100k_effdet/val/'+filename)
+        #   if len(out[index]['rois']):
+        #     for roi in out[index]['rois']:
+        #       x1,y1,x2,y2 = [int(x) for x in roi]
+        #       cv2.rectangle(ori_img, (x1,y1), (x2,y2), (255,0,0), 1)
+        #   cv2.imwrite(filename, ori_img)
+
+        for i in range(annot.size(0)):
+            seen += 1
+            labels = annot[i]
+            labels = labels[labels[:, 4] != -1]
+
+            ou = out[i]
+            nl = len(labels)
+
+            pred = np.column_stack([ou['rois'], ou['scores']])
+            pred = np.column_stack([pred, ou['class_ids']])
+            pred = torch.from_numpy(pred).cuda()
+
+            target_class = labels[:, 4].tolist() if nl else []  # target class
+
+            if len(pred) == 0:
+                if nl:
+                    stats.append((torch.zeros(0, num_thresholds, dtype=torch.bool),
+                                  torch.Tensor(), torch.Tensor(), target_class))
+                # print("here")
+                continue
+
+            if nl:
+                pred[:, :4] = scale_coords(imgs[i][1:], pred[:, :4], shapes[i][0], shapes[i][1])
+
+                labels = scale_coords(imgs[i][1:], labels, shapes[i][0], shapes[i][1])
+
+                # ori_img = cv2.imread('datasets/bdd100k_effdet/val/' + filenames[i],
+                #                      cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION | cv2.IMREAD_UNCHANGED)
+                # for label in labels:
+                #     x1, y1, x2, y2 = [int(x) for x in label[:4]]
+                #     ori_img = cv2.rectangle(ori_img, (x1, y1), (x2, y2), (255, 0, 0), 1)
+                # for pre in pred:
+                #     x1, y1, x2, y2 = [int(x) for x in pre[:4]]
+                #     # ori_img = cv2.putText(ori_img, str(pre[4].cpu().numpy()), (x1 - 10, y1 - 10),
+                #     #                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                #     ori_img = cv2.rectangle(ori_img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+
+                # cv2.imwrite('pre+label-{}.jpg'.format(filenames[i]), ori_img)
+                correct = process_batch(pred, labels, iou_thresholds)
+                if plots:
+                    confusion_matrix.process_batch(pred, labels)
+            else:
+                correct = torch.zeros(pred.shape[0], num_thresholds, dtype=torch.bool)
+            stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), target_class))
+
+            # print(stats)
+
+            # Visualization
+            # seg_0 = segmentation[i]
+            # # print('bbb', seg_0.shape)
+            # seg_0 = torch.argmax(seg_0, dim = 0)
+            # # print('before', seg_0.shape)
+            # seg_0 = seg_0.cpu().numpy()
+            #     #.transpose(1, 2, 0)
+            # # print(seg_0.shape)
+            # anh = np.zeros((384,640,3))
+            # anh[seg_0 == 0] = (255,0,0)
+            # anh[seg_0 == 1] = (0,255,0)
+            # anh[seg_0 == 2] = (0,0,255)
+            # anh = np.uint8(anh)
+            # cv2.imwrite('segmentation-{}.jpg'.format(filenames[i]),anh)
+
+        for i in range(len(params['seg_list']) + 1):
+            # print(segmentation[:,i,...].unsqueeze(1).size())
+            tp_seg, fp_seg, fn_seg, tn_seg = smp_metrics.get_stats(segmentation[:, i, ...].unsqueeze(1).cuda(),
+                                                                    seg_annot[:, i, ...].unsqueeze(
+                                                                        1).round().long().cuda(),
+                                                                    mode='binary', threshold=0.5)
+
+            iou = smp_metrics.iou_score(tp_seg, fp_seg, fn_seg, tn_seg).mean()
+            # print("I", i , iou)
+            f1 = smp_metrics.f1_score(tp_seg, fp_seg, fn_seg, tn_seg).mean()
+
+            iou_ls[i].append(iou.detach().cpu().numpy())
+            f1_ls[i].append(f1.detach().cpu().numpy())
+
+    # print(len(iou_ls[0]))
+    # print(iou_ls)
+    iou_score = np.mean(iou_ls)
+    # print(iou_score)
+    f1_score = np.mean(f1_ls)
+
+    for i in range(len(params['seg_list']) + 1):
+        iou_ls[i] = np.mean(iou_ls[i])
+        f1_ls[i] = np.mean(f1_ls[i])
+
+    # Compute statistics
+    stats = [np.concatenate(x, 0) for x in zip(*stats)]
+    # print(stats[3])
+
+    # Count detected boxes per class
+    # boxes_per_class = np.bincount(stats[2].astype(np.int64), minlength=1)
+
+    ap50 = None
+    save_dir = 'abc'
+    names = {
+        0: 'car'
+    }
+    # Compute metrics
+    if len(stats) and stats[0].any():
+        p, r, f1, ap, ap_class = ap_per_class(*stats, plot=plots, save_dir=save_dir, names=names)
+        ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
+        mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+        nt = np.bincount(stats[3].astype(np.int64), minlength=1)  # number of targets per class
+    else:
+        nt = torch.zeros(1)
+
+    # Print results
+    print(s)
+    pf = '%15s' + '%11i' * 2 + '%11.3g' * 10  # print format
+    print(pf % ('all', seen, nt.sum(), mp, mr, map50, map, iou_score, f1_score,
+                iou_ls[1], f1_ls[1], iou_ls[2], f1_ls[2]))
+
+    # Print results per class
+    verbose = True
+    training = False
+    nc = 1
+    if (verbose or (nc < 50 and not training)) and nc > 1 and len(stats):
+        for i, c in enumerate(ap_class):
+            print(pf % (names[c], seen, nt[c], p[i], r[i], ap50[i], ap[i]))
+
+    # Plots
+    if plots:
+        confusion_matrix.plot(save_dir=save_dir, names=list(names.values()))
+        confusion_matrix.tp_fp()
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-p', '--project', type=str, default='coco', help='project file that contains parameters')
+    ap.add_argument('-c', '--compound_coef', type=int, default=0, help='coefficients of efficientdet')
+    ap.add_argument('-w', '--weights', type=str, default=None, help='/path/to/weights')
+    args = ap.parse_args()
+
+    compound_coef = args.compound_coef
+    project_name = args.project
+    weights_path = f'weights/efficientdet-d{compound_coef}.pth' if args.weights is None else args.weights
+
+    params = yaml.safe_load(open(f'projects/{project_name}.yml'))
+    obj_list = params['obj_list']
+    print(params)
+
+    valid_dataset = BddDataset(
+        cfg=cfg,
+        is_train=False,
+        inputsize=cfg.MODEL.IMAGE_SIZE,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            )
+        ])
+    )
+
+    val_generator = DataLoaderX(
+        valid_dataset,
+        batch_size=2,
+        shuffle=False,
+        num_workers=cfg.WORKERS,
+        pin_memory=cfg.PIN_MEMORY,
+        collate_fn=AutoDriveDataset.collate_fn
+    )
+
+    model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(params['obj_list']),
+                                     ratios=eval(params['anchors_ratios']), scales=eval(params['anchors_scales']),
+                                     seg_classes=len(params['seg_list']))
+    try:
+        model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+    except:
+        model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu'))['model'])
+    model.requires_grad_(False)
+
+    if params['num_gpus'] > 0:
+        model.cuda()
+
+    val_from_cmd(model, val_generator, params)
