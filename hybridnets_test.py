97abaa1d43da1d0a080bc33e2338d10245c515da
Xoi Ga <xoiga123@outlook.com.vn>
2022-02-08 18:01:28 +0700

    video demo

--- hybridnets_test.py
+++ hybridnets_test.py
@@ -1,4 +1,5 @@
 import time
+from numpy.lib.type_check import imag
 import torch
 from torch.backends import cudnn
 from matplotlib import colors
@@ -6,49 +7,72 @@ from backbone import HybridNetsBackbone
 import cv2
 import numpy as np
 import glob
-from utils.utils import preprocess, invert_affine, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, plot_one_box, BBoxTransform, ClipBoxes
+from utils.utils import letterbox, scale_coords, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, \
+    plot_one_box, BBoxTransform, ClipBoxes
+import os
+from torchvision import transforms
 
 compound_coef = 3
-force_input_size = None  # set None to use default size
 img_path = [path for path in glob.glob('./demo_imgs/*.jpg')]
-img_path = [img_path[0]]  # demo with 1 image
+# img_path = [img_path[0]]  # demo with 1 image
+input_imgs = []
+shapes = []
 
 # replace this part with your project's anchor config
 anchor_ratios = [(0.62, 1.58), (1.0, 1.0), (1.58, 0.62)]
-anchor_scales = [2**0, 2**0.70, 2**1.32]
+anchor_scales = [2 ** 0, 2 ** 0.70, 2 ** 1.32]
 
 threshold = 0.5
 iou_threshold = 0.3
+imshow = False
+imwrite = True
 
 use_cuda = True
 use_float16 = False
 cudnn.fastest = True
 cudnn.benchmark = True
 
-obj_list= ['car']
-
+obj_list = ['car']
 
 color_list = standard_to_bgr(STANDARD_COLORS)
-# tf bilinear interpolation is different from any other's, just make do
-input_sizes = [512, 640, 768, 640, 1024, 1280, 1280, 1536, 1536]
-input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
-ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
-# ori_img = ori_imgs[0]
+ori_imgs = [cv2.imread(i, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION) for i in img_path]
+ori_imgs = [cv2.cvtColor(i, cv2.COLOR_BGR2RGB) for i in ori_imgs]
+# cv2.imwrite('ori.jpg', ori_imgs[0])
+# cv2.imwrite('normalized.jpg', normalized_imgs[0]*255)
+resized_shape = 640
+normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    normalize,
+])
+for ori_img in ori_imgs:
+    h0, w0 = ori_img.shape[:2]  # orig hw
+    r = resized_shape / max(h0, w0)  # resize image to img_size
+    input_img = cv2.resize(ori_img, (int(w0 * r), int(h0 * r)), interpolation=cv2.INTER_AREA)
+    h, w = input_img.shape[:2]
+
+    (input_img, _, _), ratio, pad = letterbox((input_img, input_img.copy(), input_img.copy()), resized_shape, auto=True,
+                                              scaleup=False)
+
+    input_imgs.append(input_img)
+    # cv2.imwrite('input.jpg', input_img * 255)
+    shapes.append(((h0, w0), ((h / h0, w / w0), pad)))  # for COCO mAP rescaling
 
 if use_cuda:
-    x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+    x = torch.stack([transform(fi).cuda() for fi in input_imgs], 0)
 else:
-    x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
+    x = torch.stack([transform(fi) for fi in input_imgs], 0)
 
-x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
-print(x.shape)
-
+x = x.to(torch.float32 if not use_float16 else torch.float16)
+# print(x.shape)
 model = HybridNetsBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
-                             ratios=anchor_ratios, scales=anchor_scales, seg_classes=2)
+                           ratios=anchor_ratios, scales=anchor_scales, seg_classes=2)
 try:
-    model.load_state_dict(torch.load('weights/weight97.pth', map_location='cpu'))
+    model.load_state_dict(torch.load('weights/weight.pth'), strict=False)
 except:
-    model.load_state_dict(torch.load('weights/weight97.pth', map_location='cpu')['model'])
+    model.load_state_dict(torch.load('weights/weight.pth')['model'], strict=False)
 model.requires_grad_(False)
 model.eval()
 
@@ -59,116 +83,67 @@ if use_float16:
 
 with torch.no_grad():
     features, regression, classification, anchors, seg = model(x)
-    # print(ori_imgs)
-    # ori_img = np.asarray(ori_imgs)
-    # print(ori_img.size())
-    # ratio = 640 / 1280
-    # da_predict = seg[:, :, 0:(720 - 0), 0:(1280 - 0)]
-    # print(seg.shape)
 
-    # print(da_predict.shape)
-    da_seg_mask = torch.nn.functional.interpolate(seg, size = [720,1280], mode='bilinear')
-    # print(da_seg_mask.shape)
-
+    seg = seg[:, :, 12:372, :]
+    da_seg_mask = torch.nn.functional.interpolate(seg, size=[720, 1280], mode='nearest')
     _, da_seg_mask = torch.max(da_seg_mask, 1)
-
-    # seg = torch.rand((1, 384, 640))
-    # da_seg_mask = Activation('sigmoid')(da_seg_mask)
-    # print(da_seg_mask.shape)
-    color_mask_ls = []
     for i in range(da_seg_mask.size(0)):
-    #   print(i)
-      da_seg_mask_ = da_seg_mask[i].squeeze().cpu().numpy().round()
-      # da_seg_mask = torch.argmax(da_seg_mask, dim = 0)
-      # da_seg_mask[da_seg_mask < 0.5] = 0
-      # da_seg_mask[da_seg_mask >= 0.5] = 1
+        #   print(i)
+        da_seg_mask_ = da_seg_mask[i].squeeze().cpu().numpy().round()
+        color_area = np.zeros((da_seg_mask_.shape[0], da_seg_mask_.shape[1], 3), dtype=np.uint8)
+        color_area[da_seg_mask_ == 1] = [0, 255, 0]
+        color_area[da_seg_mask_ == 2] = [0, 0, 255]
+        color_seg = color_area[..., ::-1]
+        # cv2.imwrite('seg_only_{}.jpg'.format(i), color_seg)
 
-      color_area = np.zeros((da_seg_mask_.shape[0], da_seg_mask_.shape[1], 3), dtype=np.uint8)
-
-      # for label, color in enumerate(palette):
-      #     color_area[result[0] == label, :] = color
-
-      color_area[da_seg_mask_ == 1] = [0, 255, 0]
-      color_area[da_seg_mask_ == 2] = [0, 0, 255]
-
-      color_seg = color_area[..., ::-1]
-    #   print(color_seg.shape)
-
-      cv2.imwrite('seg_only_{}.jpg'.format(i),color_seg)
-
-
-      # convert to BGR
-      # color_seg = color_seg[..., ::-1]
-      # # print(color_seg.shape)
-      color_mask = np.mean(color_seg, 2)
-      ori_img = ori_imgs[i]
-    #   print(ori_img.shape)
-      # ori_img = cv2.resize(ori_img, (1280, 768), interpolation=cv2.INTER_LINEAR)
-      ori_img[color_mask != 0] = ori_img[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
-      # img = img * 0.5 + color_seg * 0.5
-      ori_img = ori_img.astype(np.uint8)
-      # img = cv2.resize(ori_img, (1280, 720), interpolation=cv2.INTER_LINEAR)
-      cv2.imwrite('seg_{}.jpg'.format(i), ori_img)
-      # cv2.waitKey(0)
-      color_mask_ls.append(color_mask)
+        color_mask = np.mean(color_seg, 2)
+        ori_img = ori_imgs[i]
+        ori_img[color_mask != 0] = ori_img[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
+        ori_img = ori_img.astype(np.uint8)
+        # cv2.imwrite('seg_{}.jpg'.format(i), ori_img)
 
     regressBoxes = BBoxTransform()
     clipBoxes = ClipBoxes()
-
-    # print(x.shape)
-
     out = postprocess(x,
                       anchors, regression, classification,
                       regressBoxes, clipBoxes,
                       threshold, iou_threshold)
 
-
-def display(preds, imgs, imshow=True, imwrite=False):
-    global color_seg
-    global color_mask
-
-    for i in range(len(imgs)):
-        if len(preds[i]['rois']) == 0:
+    for i in range(len(ori_imgs)):
+        if len(out[i]['rois']) == 0:
             continue
 
-        imgs[i] = imgs[i].copy()
-
-        for j in range(len(preds[i]['rois'])):
-            x1, y1, x2, y2 = preds[i]['rois'][j].astype(np.int)
-            obj = obj_list[preds[i]['class_ids'][j]]
-            score = float(preds[i]['scores'][j])
-            plot_one_box(imgs[i], [x1, y1, x2, y2], label=obj,score=score,color=color_list[get_index_label(obj, obj_list)])
-
-        # imgs[i] = cv2.resize(imgs[i], (1280, 768), interpolation=cv2.INTER_LINEAR)
-        imgs[i][color_mask_ls[i] != 0] = imgs[i][color_mask_ls[i] != 0] * 0.5 + color_seg[color_mask_ls[i] != 0] * 0.5
-        # imgs[i] = cv2.resize(imgs[i], (1280, 720), interpolation=cv2.INTER_LINEAR)
+        ori_imgs[i] = ori_imgs[i].copy()
+        out[i]['rois'] = scale_coords(ori_imgs[i][:2], out[i]['rois'], shapes[i][0], shapes[i][1])
+        for j in range(len(out[i]['rois'])):
+            x1, y1, x2, y2 = out[i]['rois'][j].astype(np.int)
+            obj = obj_list[out[i]['class_ids'][j]]
+            score = float(out[i]['scores'][j])
+            plot_one_box(ori_imgs[i], [x1, y1, x2, y2], label=obj, score=score,
+                         color=color_list[get_index_label(obj, obj_list)])
 
         if imshow:
-            cv2.imshow('img', imgs[i])
+            cv2.imshow('img', ori_imgs[i])
             cv2.waitKey(0)
 
         if imwrite:
-            # print('inside')
-            cv2.imwrite(f'test/{i}.jpg', imgs[i])
+            os.makedirs('test', exist_ok=True)
+            cv2.imwrite(f'test/{i}.jpg', cv2.cvtColor(ori_imgs[i], cv2.COLOR_RGB2BGR))
 
-
-out = invert_affine(framed_metas, out)
-display(out, ori_imgs, imshow=False, imwrite=True)
-
+# exit()
 print('running speed test...')
 with torch.no_grad():
     print('test1: model inferring and postprocessing')
     print('inferring image for 10 times...')
+    x = x[0, ...]
     t1 = time.time()
     for _ in range(10):
         _, regression, classification, anchors, segmentation = model(x)
-
 
         out = postprocess(x,
                           anchors, regression, classification,
                           regressBoxes, clipBoxes,
                           threshold, iou_threshold)
-        out = invert_affine(framed_metas, out)
 
     t2 = time.time()
     tact_time = (t2 - t1) / 10
@@ -181,7 +156,7 @@ with torch.no_grad():
     x = torch.cat([x] * 32, 0)
     for _ in range(10):
         _, regression, classification, anchors, segmentation = model(x)
-    
+
     t2 = time.time()
     tact_time = (t2 - t1) / 10
     print(f'{tact_time} seconds, {32 / tact_time} FPS, @batch_size 32')
--- hybridnets_test_videos.py
+++ hybridnets_test_videos.py
@@ -1,22 +1,34 @@
+import time
+from numpy.lib.type_check import imag
 import torch
+from torch.backends import cudnn
+from matplotlib import colors
+from backbone import HybridNetsBackbone
 import cv2
 import numpy as np
-from torch.backends import cudnn
-from backbone import HybridNetsBackbone
-from utils.utils import invert_affine, postprocess, preprocess_video, BBoxTransform, ClipBoxes
+import glob
+from utils.utils import letterbox, scale_coords, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, \
+    plot_one_box, BBoxTransform, ClipBoxes
+import os
+from torchvision import transforms
 
-# Video's path
-video_src = 'videotest.mp4'  # set int to use webcam, set str to read from a video file
-
-compound_coef = 0
-force_input_size = None  # set None to use default size
+compound_coef = 3
+video_src = '1.mp4'
+video_out = 'output.mp4'
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out_stream = cv2.VideoWriter(video_out, fourcc, 30.0, (1920, 1080))
+input_imgs = []
+shapes = []
 
 # replace this part with your project's anchor config
 anchor_ratios = [(0.62, 1.58), (1.0, 1.0), (1.58, 0.62)]
-anchor_scales = [2**0, 2**0.70, 2**1.32]
+anchor_scales = [2 ** 0, 2 ** 0.70, 2 ** 1.32]
 
-threshold = 0.2
-iou_threshold = 0.2
+threshold = 0.25
+iou_threshold = 0.3
+imshow = False
+imwrite = True
 
 use_cuda = True
 use_float16 = False
@@ -25,14 +37,23 @@ cudnn.benchmark = True
 
 obj_list = ['car']
 
-# tf bilinear interpolation is different from any other's, just make do
-input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536, 1536]
-input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
+color_list = standard_to_bgr(STANDARD_COLORS)
+resized_shape = 640
+normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    normalize,
+])
+# print(x.shape)
 
-# load model
 model = HybridNetsBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
-                             ratios=anchor_ratios, scales=anchor_scales, seg_classes=2)
-model.load_state_dict(torch.load(f'weights/weight.pth'))
+                           ratios=anchor_ratios, scales=anchor_scales, seg_classes=2)
+try:
+    model.load_state_dict(torch.load('weights/weight.pth'), strict=False)
+except:
+    model.load_state_dict(torch.load('weights/weight.pth')['model'], strict=False)
 model.requires_grad_(False)
 model.eval()
 
@@ -40,68 +61,71 @@ if use_cuda:
     model = model.cuda()
 if use_float16:
     model = model.half()
-
-# function for display
-def display(preds, imgs):
-    for i in range(len(imgs)):
-        if len(preds[i]['rois']) == 0:
-            return imgs[i]
-
-        for j in range(len(preds[i]['rois'])):
-            (x1, y1, x2, y2) = preds[i]['rois'][j].astype(np.int)
-            cv2.rectangle(imgs[i], (x1, y1), (x2, y2), (255, 255, 0), 2)
-            obj = obj_list[preds[i]['class_ids'][j]]
-            score = float(preds[i]['scores'][j])
-
-            cv2.putText(imgs[i], '{}, {:.3f}'.format(obj, score),
-                        (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        (255, 255, 0), 1)
-        
-        return imgs[i]
-# Box
-regressBoxes = BBoxTransform()
-clipBoxes = ClipBoxes()
-
-# Video capture
 cap = cv2.VideoCapture(video_src)
-
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # frame preprocessing
-    ori_imgs, framed_imgs, framed_metas = preprocess_video(frame, max_size=input_size)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    h0, w0 = frame.shape[:2]  # orig hw
+    r = resized_shape / max(h0, w0)  # resize image to img_size
+    input_img = cv2.resize(frame, (int(w0 * r), int(h0 * r)), interpolation=cv2.INTER_AREA)
+    h, w = input_img.shape[:2]
+
+    (input_img, _, _), ratio, pad = letterbox((input_img, input_img.copy(), input_img.copy()), resized_shape, auto=True,
+                                              scaleup=False)
+
+    shapes = ((h0, w0), ((h / h0, w / w0), pad))
 
     if use_cuda:
-        x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+        x = transform(input_img).cuda()
     else:
-        x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
+        x = transform(input_img)
 
-    x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
-
-    # model predict
+    x = x.to(torch.float32 if not use_float16 else torch.float16)
+    x.unsqueeze_(0)
     with torch.no_grad():
-        features, regression, classification, anchors = model(x)
+        features, regression, classification, anchors, seg = model(x)
 
+        seg = seg[:, :, 12:372, :]
+        da_seg_mask = torch.nn.functional.interpolate(seg, size=[h0, w0], mode='nearest')
+        _, da_seg_mask = torch.max(da_seg_mask, 1)
+        da_seg_mask_ = da_seg_mask[0].squeeze().cpu().numpy().round()
+
+        color_area = np.zeros((da_seg_mask_.shape[0], da_seg_mask_.shape[1], 3), dtype=np.uint8)
+        color_area[da_seg_mask_ == 1] = [0, 255, 0]
+        color_area[da_seg_mask_ == 2] = [0, 0, 255]
+        color_seg = color_area[..., ::-1]
+
+        # cv2.imwrite('seg_only_{}.jpg'.format(i), color_seg)
+
+        color_mask = np.mean(color_seg, 2)
+        frame[color_mask != 0] = frame[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
+        frame = frame.astype(np.uint8)
+        # cv2.imwrite('seg_{}.jpg'.format(i), ori_img)
+
+        regressBoxes = BBoxTransform()
+        clipBoxes = ClipBoxes()
         out = postprocess(x,
-                        anchors, regression, classification,
-                        regressBoxes, clipBoxes,
-                        threshold, iou_threshold)
+                          anchors, regression, classification,
+                          regressBoxes, clipBoxes,
+                          threshold, iou_threshold)
 
-    # result
-    out = invert_affine(framed_metas, out)
-    img_show = display(out, ori_imgs)
+        out = out[0]
+        if len(out['rois']) == 0:
+            continue
 
-    # show frame by frame
-    cv2.imshow('frame',img_show)
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
+        out['rois'] = scale_coords(frame[:2], out['rois'], shapes[0], shapes[1])
+        for j in range(len(out['rois'])):
+            x1, y1, x2, y2 = out['rois'][j].astype(np.int)
+            obj = obj_list[out['class_ids'][j]]
+            score = float(out['scores'][j])
+            plot_one_box(frame, [x1, y1, x2, y2], label=obj, score=score,
+                         color=color_list[get_index_label(obj, obj_list)])
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        out_stream.write(frame)
 
 cap.release()
-cv2.destroyAllWindows()
-
-
-
-
-
+out_stream.release()
